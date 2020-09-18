5bf232540f7e69c3a527e1b20536f813c3625e5f
Christian den Boer <csdenboer@Christians-MacBook-Pro.local>
2020-09-18 17:03:40 +0200

    updated version from 1.0 to 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = '2020, Christian den Boer'
 author = 'Christian den Boer'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
